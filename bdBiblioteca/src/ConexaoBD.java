e560360b2cc63daafa5008679418d32bd1b8a34b
Júlio Pacheco Stein <juliopstein@gmail.com>
2024-11-07 11:04:09 -0300

    Minhas informações do bd

--- bdBiblioteca/src/ConexaoBD.java
+++ bdBiblioteca/src/ConexaoBD.java
@@ -6,10 +6,10 @@ public class ConexaoBD {
 
     private static final String
             URL = "jdbc:sqlserver://regulus.cotuca.unicamp.br:1433;"+
-                  "databaseName=chico"+
+                  "databaseName=BD24137"+
             ";integratedSecurity=false;encrypt=true;trustServerCertificate=true";
-    private static final String USER = "chico";
-    private static final String PASSWORD = "82Eridani";
+    private static final String USER = "BD24137";
+    private static final String PASSWORD = "Laracookiefub@";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
